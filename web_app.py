65335a4fc352388cde58e1fb6139c965a825e3b7
Marcus Bragion <viniciusbragion@outlook.pt>
2024-08-01 21:06:51 -0300

    feat: web_app atualizado

--- web_app.py
+++ web_app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import pandas as pd
+from whatsapp_bulk_messaging import send_whatsapp
 
 
-uploaded_file = st.file_uploader("Upload a file")
-if uploaded_file is not None:
+def main():
+    st.title("Programa para enviar mensagens via WhatsApp Web")
+    uploaded_file = st.file_uploader("Upload a file")
+    if uploaded_file is not None:
+        df = pd.read_excel(uploaded_file, dtype={"TELEFONE": str})
+
+    send_whatsapp(df, msg="Olá, Marcus")
+
+if __name__ == "__main__":
+    main()
     
-    df = pd.read_excel(uploaded_file, dtype={"TELEFONE": str})
-    
-    st.write(df)
-
 
